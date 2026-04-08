8f323785c523e36233e5137445bffbb224ebfb36
Nico Raag <n.raag@proton.me>
2026-04-08 22:33:36 +0200

    read links and rect/position from json

--- add_hyperlink.py
+++ add_hyperlink.py
@@ -1,7 +1,8 @@
-import argparse
-import os
+import argparse, os
 from pypdf import PdfReader, PdfWriter
 from pypdf.annotations import Link
+import json
+from pprint import pprint
 
 # Function to check that file given as argument acutally exists
 def existing_file(path):
@@ -15,6 +16,10 @@ parser = argparse.ArgumentParser()
 # add pdf_file as positional argument (with help-text), type parameter calls
 # existing_file function during parsing to check that file actually exists
 parser.add_argument("pdf_file", help="path to pdf file", type=existing_file)
+# json input file containing coordinates and corresponding links/urls, optional
+# (nargs='?') postitional argument, default value, check taht file actually exists
+parser.add_argument("json_file", help="path to input json file (default: \"annot_squares.json\")",
+                    nargs='?', default="annot_squares.json", type=existing_file)
 args = parser.parse_args()
 
 # Fill the writer with the pages you want
@@ -23,22 +28,15 @@ page = reader.pages[0]
 writer = PdfWriter()
 writer.add_page(page)
 
-# Define hyperlinks and positions
-# TODO: Read this from json input file
-annotations = [
-    {
-        "rect": (461, 787, 566, 798),
-        "url": "mailto:nicolausjanos.raag@iths.se",
-    },
-    {
-        "rect": (461, 745, 524, 756),
-        "url": "https://github.com/njrg",
-    },
-    {
-        "rect": (461, 714, 558, 734),
-        "url": "https://linkedin.com/in/nicolaus-janos-raag-65613a104",
-    },
-]
+# Read hyperlinks and positions from json input file
+# (containing two key-value pairs per entry, "rect" with the list-value containing
+# the four corners coordinates, and the "url" key
+with open(args.json_file, 'r') as fj:
+    annotations = json.load(fj) # load the json data into a dictionary
+
+print(f"The following hyperlinks will be added at the corresponding positions in \"{args.pdf_file}\"")
+print()
+pprint(annotations)
 
 # Add the annotations (hyperlinks)
 for a in annotations:
